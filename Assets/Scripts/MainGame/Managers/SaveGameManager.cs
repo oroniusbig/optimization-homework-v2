b760b5cf1a713bb02e4c1834b255c951ea1b05e2
emilx10 <emilxt12@gmail.com>
2025-02-24 18:04:24 +0200

    Optimize
    Optimized the project further by deleting the unnecessary top level statements and making a const string to hold the player tag.

--- Assets/Scripts/MainGame/Managers/SaveGameManager.cs
+++ Assets/Scripts/MainGame/Managers/SaveGameManager.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
 using UnityEngine;
 
 public class SaveGameManager : MonoBehaviour
